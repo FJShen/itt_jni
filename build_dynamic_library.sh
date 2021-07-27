94d2afd7032030b33623c5a2d37e20660c63a5b9
Fangjia Shen <shen449@tgrogers-pc01>
2021-07-27 16:27:29 -0400

    second commit

--- build_dynamic_library.sh
+++ build_dynamic_library.sh
@@ -3,7 +3,7 @@
 #javac is done by maven
 #javac -source 1.8 -target 1.8 -cp src/main/java com/fangjia/itt/MyJniClass.java
 
-javah -classpath src/main/java com.fangjia.itt.ITT
+javah -classpath target/classes com.fangjia.itt.ITT
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/ 
 export VTUNE_HOME=/opt/intel/oneapi/vtune/latest/ 
-gcc com_fangjia_itt_ITT.c -Wall -g -fPIC -pthread -ldl -shared -I$VTUNE_HOME/sdk/include -L$VTUNE_HOME/sdk/lib64 -I$JAVA_HOME/include -I$JAVA_HOME/include/linux -L/home/shen449/ittapi/build_linux2/64/bin -I/home/shen449/ittapi/include -littnotify -o libittjni.so 
+gcc com_fangjia_itt_ITT.c -Wall -g -fPIC -fstack-protector -pthread -ldl -shared -I$VTUNE_HOME/sdk/include -L$VTUNE_HOME/sdk/lib64 -I$JAVA_HOME/include -I$JAVA_HOME/include/linux -L/home/shen449/ittapi/build_linux2/64/bin -I/home/shen449/ittapi/include -littnotify -o libittjni.so 
